bfb57449f9bb945afbfed1adae378050eb0349bb
Johannes Lauter <johannes.lauter@sparwelt.de>
2019-07-29 13:49:04 +0200

    ⬆️ vuetify 2.0

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,17 +1,36 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import colors from 'vuetify/es5/util/colors'
+import colors from 'vuetify/lib/util/colors'
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
-Vue.use(Vuetify, {
-  iconfont: 'md',
-  theme: {
-    primary: '#121212', // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
-  }
-})
+Vue.use(Vuetify)
+
+export default (ctx) => {
+  const vuetify = new Vuetify({
+    icons: {
+      iconfont: 'md', // default - only for display purposes
+    },
+    theme: {
+      options: {
+        minifyTheme: css => {
+          return process.env.NODE_ENV === 'production'
+            ? css.replace(/[\s|\r\n|\r|\n]/g, '')
+            : css
+        },
+      },
+      themes: {
+        light: {
+          secondary: colors.grey.darken1,
+          accent: colors.shades.black,
+          error: colors.red.accent3,
+        },
+        dark: {
+          primary: colors.blue.lighten3,
+        },
+      }
+    }
+  })
+
+  ctx.app.vuetify = vuetify
+  ctx.$vuetify = vuetify.framework
+}
